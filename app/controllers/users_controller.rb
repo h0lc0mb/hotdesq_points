287d6c6f78a0056c2f01439543b7986fdba1ece6
Susan Holcomb <thefurtheroff@gmail.com>
2017-10-28 02:27:40 -0700

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Hot DesQ Points App, Mate."
+      log_in @user
+      flash[:success] = "Welcome to the Hot DesQ Points App, mate."
       redirect_to @user
     else
       render 'new'
