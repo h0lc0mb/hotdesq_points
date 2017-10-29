77ca0a629d5349faffd22cf8c8c536e468046048
Susan Holcomb <thefurtheroff@gmail.com>
2017-10-29 04:09:01 -0700

    Finish user edit, update actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,43 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:twizzler)
+    @other_user = users(:ninja)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
   end
 
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email,
+                                              startup: @user.startup } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect show when not logged in" do
+    get user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              "password",
+                                            password_confirmation: "password",
+                                            admin: true } }
+    assert_not @other_user.reload.admin?
+  end
 end
